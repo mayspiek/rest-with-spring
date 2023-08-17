aa8197917c164e49f3920626f1c9aeeeac45cc3c
Mayara Spieker Carvalho <maya.spieker@gmail.com>
2023-08-17 11:56:40 -0300

    Refatoração de Person Controller

--- src/main/java/br/com/mayara/controllers/PersonController.java
+++ src/main/java/br/com/mayara/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.com.mayara.model.Person;
 import br.com.mayara.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,36 +14,31 @@ import java.util.List;
 public class PersonController {
     @Autowired
     private PersonServices service;
-    // private PersonServices service = new PersonServices();
-    @RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() throws Exception {
         return service.findAll();
     }
-     @RequestMapping(value="/{id}",
-             method=RequestMethod.GET,
-             produces = MediaType.APPLICATION_JSON_VALUE)
+     @GetMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
      public Person findById(@PathVariable(value="id")Long id) throws Exception {
         return service.findById(id);
     }
 
-    @RequestMapping(method=RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) throws Exception {
         return service.create(person);
     }
 
-    @RequestMapping(method=RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person) throws Exception {
         return service.update(person);
     }
 
-    @RequestMapping(value="/{id}",
-            method=RequestMethod.DELETE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public void delete(@PathVariable(value="id")Long id) throws Exception {
+    @DeleteMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable(value="id")Long id) throws Exception {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
+
+    //a espcificação de consumo e produção de dados é opcional, mas nesse caso precisamos por causa do swagger
 }
