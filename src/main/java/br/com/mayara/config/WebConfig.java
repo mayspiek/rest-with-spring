e93212dc9888cdda95f107e3fdc2869eda14d23e
Mayara Spieker Carvalho <maya.spieker@gmail.com>
2023-09-29 11:58:29 -0300

    Habilitando CORS de forma Global

--- src/main/java/br/com/mayara/config/WebConfig.java
+++ src/main/java/br/com/mayara/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.mayara.config;
 
 import br.com.mayara.serialization.converter.YamlJacksonToHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,10 +14,20 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
-
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJacksonToHttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
